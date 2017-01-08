f8772f41148fe3a88a06def2ff2a2646eed6060b
stascrash <stascrash@gmail.com>
2017-01-08 16:32:01 +0100

    Added solution for read_more Quiz

--- read_more.py
+++ read_more.py
@@ -1,5 +1,7 @@
+# https://www.codeeval.com/open_challenges/167/
 import sys
-test_cases = open(sys.argv[1], 'r')
+# test_cases = open(sys.argv[1], 'r')
+test_cases = open('read_more.txt', 'r')
 test_lines = (line.rstrip() for line in test_cases)
 
 maxLength = 55
@@ -7,9 +9,11 @@ trimLine = 40
 readMore = "... <Read More>"
 
 for test in test_lines:
-	if len(test) >= maxLength:
-		trimmed = test[:trimLine].rstrip()
-		trimmed += readMore
+	if len(test) > maxLength:
+		trimmed = test[:trimLine]
+		trimmed = trimmed.rsplit(' ', 1)[0] + readMore
 		test = trimmed
-	print(test)
+	sys.stdout.write(test)
+	sys.stdout.write("\n")
+	sys.stdout.flush()
 test_cases.close()
